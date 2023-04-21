ab8757c82034b137f72eb6dd4b68eb8fc54910bb
connorwarme <connor.warme@gmail.com>
2023-04-21 15:58:24 -0600

    Adjust to user (singular), some styling on signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ exports.user_detail = asyncHandler(async (req, res, next) => {
   res.send("not implemented: post detail")
 })
 exports.user_create_get = asyncHandler(async (req, res, next) => {
-  res.send("not implemented: user create get")
+  res.render("sign-up", {title: "Sign Up"})
 })
 exports.user_create_post = asyncHandler(async (req, res, next) => {
   res.send("not implemented: user create post")
