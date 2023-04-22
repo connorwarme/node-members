331a36096b594834d1b7787e3b678abc64aa4be6
connorwarme <connor.warme@gmail.com>
2023-04-21 18:17:25 -0600

    Sign up view - add errors, debug

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,19 +13,19 @@ exports.user_create_get = asyncHandler(async (req, res, next) => {
   res.render("sign-up", {title: "Sign Up"})
 })
 exports.user_create_post = [
-  body("first_name", "Please add your first name")
+  body("first_name", "Please add your first name.")
     .trim()
     .isLength({ min: 1 })
     .escape(),
-  body("family_name", "Please add your family name")
+  body("family_name", "Please add your family name.")
     .trim()
     .isLength({ min: 1 })
     .escape(),
-  body("email", "Please add your email address")
+  body("email", "Please add your email address.")
     .trim()
     .isLength({ min: 1 })
     .isEmail()
-    .withMessage("Must be a valid email address")
+    .withMessage("Must be a valid email address!")
     .escape(),
   
   asyncHandler(async (req, res, next) => {
@@ -47,7 +47,7 @@ exports.user_create_post = [
       // check if email is already in use
       const emailExists = await User.findOne({ email: req.body.email }).exec()
       if (emailExists) {
-        const error = new Error("Email address already associated with an account")
+        const error = new Error("Email address already associated with an account!")
         error.status = 404
         res.render("sign-up", {
           title: "Sign Up",
